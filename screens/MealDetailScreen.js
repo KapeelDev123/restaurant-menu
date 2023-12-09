8f66a2742fb27217e5f9addebc1e0575ad55e869
KapeelDev123 <kapeeldevkhatri@gmail.com>
2023-12-09 11:44:23 +0500

    changes made

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -19,7 +19,7 @@ function MealDetailScreen({ route, navigation }) {
 
   const selectedMeal = MEALS.find((meal) => meal.id === mealId)
 
-  const mealIsFavorite = favoriteMealIds.ids.includes(mealId)
+  const mealIsFavorite = favoriteMealIds.includes(mealId)
 
   function changeFavoriteStatusHandler() {
     if (mealIsFavorite) {
